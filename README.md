ee4f4ef212a978b748b85f52f7a2b9d8521c3fc5
Jason Rosello <rosello.jason@gmail.com>
2023-08-22 07:26:47 -0500

    filled out readme, added structure to audiolooper module

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # audiolooper
-Audio Looper to be used with Raspberry Pi Pico--not quite ready yet
+This repo is in progress.  When finished, the Audio Looper will be designed for use with to be used with Raspberry Pi Pico in main.py.  At the same time, the modules should be useable for anyone who knows how to code drivers from USB or the sound card directly on Python.
+
+The first edition of this should include the following features:
+
+ - An audio stream bypass
+ - A visual display
+ - Audio layering
+ - Start, stop, restart, pause, continue playback as a whole and for layer
+ - Multiple audio inputs
+ - Multiple footswitches
+ - Adjustable output volume (overall) + layer volume
+ - Audio bypass (for solo, etc)
+ 
+ Those unfamiliar with the concept of an audio looper may find this video helpful as a demonstration:
+ 
+ https://youtu.be/0yK6S6qjj4A
--- audiolooper.py
+++ audiolooper.py
@@ -6,24 +6,71 @@
 # - adjustable output volume (overall) + layer volume
 # - audio bypass (for solo, etc)
 
+#audioloop class contains the loop properties
 
 class audioloop:
-    def __init__(self,audio_input, footswitch_input, label,length,output_volume,speed):
+    def __init__(self,audio_input, label,length):
         self.audio_input = audio_input
-        self.footswitch_input = footswitch_input
         self.label = label
-        self.length= length
-        self.output_volume= output_volume
-        self.speed = speed
+        self.length = length
+
+#audioaction takes the input from the switches, instantiates a new loop, and does something with it.  Main functionality with the loops are in this class
+
+class audioaction:
+    def __init__(self,audioloop,footswitch_input):
+        self.audioloop = audioloop
+        self.footswitch_input= footswitch_input
+        
+    def create_layer():
+        pass
+    
+    def end_layer():
+        pass
+    
+    def play(layers):
+        pass
+    
+    def pause(layers):
+        pass
+    
+    #continue can only happen after a pause
+        def continue(layers):
+            pass
+    
+    def stop(layers):
+        pass
+    
+    def delete(layers):
+        pass
+    
+    def merge(layers):
+        pass
+
+#footswitch class is the interface between the user and the device.  The class itself contains the properties/interactions of the footswitch
 
 class footswitch:
-    def __init__():
-        pass
+    def __init__(self, label, hold_time,taps):
+        self.label = label
+        self.hold_time = hold_time
+        self.taps = taps
 
-class loopdisplay():
-    def __init__():
-        pass
+#loopdisplay class holds the message sent to the display.  The input message will depend on the data sent from the footswitches as compared to the loop scenarios overall (how many audio loops, etc).  The output message will need to 'translate' the representation of the scenario based on the type of display used--be it LED's, 7 segment, multi line, etc.
 
-class audioinput():
-    def __init__():
+class loopdisplay:
+    def __init__(self, input_message, display_type,output_message):
+        self.message = message
+        self.input_message = input_message
+        self.display_type = display_type
+        self.output_message = output_message
+        
+    display_types = ['LED','single 7-segment','multiple 7-segment','multi-line','computer monitor','touchscreen','smartphone','tablet']
+    
+#audioinout handles/creates the input and output streams on the processing level.  Actual analog handling to be done on the main.py file with the device.  Bypass function is located here for the signal handling; it's initiated by the footswitches dependent on how the programmer would like to implement it on the device.
+
+class audioinout():
+    def __init__(self,audio_stream_in,audio_stream_out):
+        self.audio_stream_in = audio_stream_in
+        self.audio_stream_out = audio_stream_out
+    
+    def audio_bypass(signal,source,destination):
         pass
